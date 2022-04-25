dbfb0af949db1f4be3ab0f10d4810f0981423483
index.com <42189020+faiver99@users.noreply.github.com>
2022-04-25 16:30:41 -0500

    Update Ej 1 al 3.py

--- Ej 1 al 3.py
+++ Ej 1 al 3.py
@@ -2,13 +2,13 @@
 # ejercicio 1 
 print("Ejercicio 1 \n")
 
-primerNumero=int(input("ingrese el primer numero :" ));
-segundoNumero=int(input("ingrese el segundo numero :" ));
-tercerNumero=int(input("ingrese el tercero numero :" ));
+primerNumero=int(input("ingrese el primer numero:" ));
+segundoNumero=int(input("ingrese el segundo numero:" ));
+tercerNumero=int(input("ingrese el tercero numero:" ));
 
 
 if primerNumero > 10 and segundoNumero > 10 and  tercerNumero > 10: 
-    print("Todos los numeros ingresados son mayores a 10  ")
+    print("Todos los numeros ingresados son mayores a 10")
 else:
  print("Alguno(s) de los numeros ingresados no es mayor a 10 \n" )
 
@@ -16,13 +16,13 @@ else:
 # ejercicio 2
 
 print("Ejercicio 2 \n")
-primerNumero=int(input("ingrese el primer numero :" ));
-segundoNumero=int(input("ingrese el segundo numero :" ));
-tercerNumero=int(input("ingrese el tercero numero :" ));
+primerNumero=int(input("ingrese el primer numero:" ));
+segundoNumero=int(input("ingrese el segundo numero:" ));
+tercerNumero=int(input("ingrese el tercero numero:" ));
 
 
 if primerNumero > 10 or segundoNumero > 10 or  tercerNumero > 10: 
-    print("Alguno(s) de los numeros ingresados (es/son) mayores a 10  ")
+    print("Alguno(s) de los numeros ingresados (es/son) mayores a 10 ")
 else:
  print("Ninguno de los numeros ingresados es mayor a 10 \n")
 
@@ -31,13 +31,13 @@ else:
 
 print("Ejercicio 3 \n")
 
-primerNumero=int(input("ingrese el primer numero :" ));
-segundoNumero=int(input("ingrese el segundo numero :" ));
-tercerNumero=int(input("ingrese el tercero numero :" ));
+primerNumero=int(input("ingrese el primer numero:" ));
+segundoNumero=int(input("ingrese el segundo numero:" ));
+tercerNumero=int(input("ingrese el tercero numero:" ));
 
 if primerNumero == segundoNumero == tercerNumero :
     A=int(primerNumero+segundoNumero)
-    print("La Suma del primero y segundo es :", A)
+    print("La Suma del primero y segundo es:", A)
     B=tercerNumero*A
     print("la multiplicacion de los numeros sumados por el tercer numero es :", B)
 else:
@@ -48,8 +48,8 @@ else:
 
 print("Ejercicio 4 \n")
 
-salario=int(input("Ingrese el salario del trabajador :" ));
-antiguedad=int(input("Ingrese los años laborados :" ));
+salario=int(input("Ingrese el salario del trabajador:" ));
+antiguedad=int(input("Ingrese los años laborados:" ));
 
 
 if  salario < 500 and antiguedad >= 10:
@@ -59,10 +59,10 @@ if  salario < 500 and antiguedad >= 10:
 
 if salario < 500 and antiguedad <= 10:
     B=500*(1+0.05)
-    print("El sueldo a pagar es de ", B)
+    print("El sueldo a pagar es de:", B)
 
 if salario >500:
-    print("El sueldo es de", salario)
+    print("El sueldo es de:", salario)
 
 
 
